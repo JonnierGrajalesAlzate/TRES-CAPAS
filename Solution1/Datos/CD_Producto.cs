7ea0d8fd990d3303a665a13d5eee7bb862d4b882
JonniDevs <105441441+JonnierGrajalesAlzate@users.noreply.github.com>
2025-02-17 13:39:31 -0500

    Add files via upload

---
+++ Solution1/Datos/CD_Producto.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Agregar el paquete IO para el manejo de archivos
+using System.IO;
+
+namespace Datos
+{
+    public class CD_Producto
+    {
+        public string codigo {  get; set; }
+        public string nombre { get; set; }
+        public string precio { get; set; }
+        public string stock { get; set; }
+
+        //Variables para acceso y manipulación de datos (Stream)
+
+        private FileStream archivo = null; //indicar la ruta y modos
+        private StreamReader lector = null; //Stream para leer datos
+        private StreamWriter escritor = null; //Stream para escribir datos
+
+        //Establecer la ruta del archivo
+        private string ruta = @"..\..\Recursos\productos.txt";
+
+        public string mensaje = "";
+
+        //crear coleccion para almacenar todos los procutos
+
+        List<string> lista = new System.Collections.Generic.List<string>();
+
+        //Metodo (servicio) para guardar o escribir en el archivo
+        public void insertar(string codigo, string nombre, string precio, string stock){
+            //manejo de excepciones
+            try
+            {
+                //indicar cual archivo se abrira y del modo y acceso
+                archivo = new FileStream(ruta, FileMode.Append, FileAccess.Write);
+                //crear el escritor sobre el archivo
+                escritor = new StreamWriter(archivo);
+                //Almacenar los datos en el archivo
+                escritor.WriteLine(codigo + "," + nombre + "," + precio + "," + stock);
+                //Cerrar el archivo
+                escritor.Close();
+            }
+            catch (IOException ex) {
+                mensaje = "ERROR: " + ex.Message;
+            }
+        }//Cierre del método
+
+        //Metodo´para listar todos los produtos
+        public List<string> listado() {
+            //manejo de excepciones
+            try
+            {
+                //indicar cual archivo se abrira y del modo y acceso
+                archivo = new FileStream(ruta, FileMode.Open, FileAccess.Read);
+                //crear el lector
+                lector = new StreamReader(archivo);
+                string linea;
+                while ((linea = lector.ReadLine()) != null)
+                {
+                    lista.Add(linea);
+                }
+                //cerrar el lector
+                lector.Close();
+            }
+            catch (IOException ex)
+            {
+                mensaje = "ERROR: " + ex.Message;
+            }
+        }//Cierre del método
+    }
+    }
+}//Cierre de la clase
---
+++ Solution1/Presentacion/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace Presentacion
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblStock = new System.Windows.Forms.Label();
+            this.lblPrecio = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtStock = new System.Windows.Forms.TextBox();
+            this.gbInventario = new System.Windows.Forms.GroupBox();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnSalir = new System.Windows.Forms.Button();
+            this.lstListado = new System.Windows.Forms.ListBox();
+            this.gbInventario.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Location = new System.Drawing.Point(16, 26);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(43, 13);
+            this.lblCodigo.TabIndex = 1;
+            this.lblCodigo.Text = "Código:";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Location = new System.Drawing.Point(16, 51);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(47, 13);
+            this.lblNombre.TabIndex = 2;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // lblStock
+            // 
+            this.lblStock.AutoSize = true;
+            this.lblStock.Location = new System.Drawing.Point(16, 115);
+            this.lblStock.Name = "lblStock";
+            this.lblStock.Size = new System.Drawing.Size(38, 13);
+            this.lblStock.TabIndex = 3;
+            this.lblStock.Text = "Stock:";
+            // 
+            // lblPrecio
+            // 
+            this.lblPrecio.AutoSize = true;
+            this.lblPrecio.Location = new System.Drawing.Point(16, 80);
+            this.lblPrecio.Name = "lblPrecio";
+            this.lblPrecio.Size = new System.Drawing.Size(40, 13);
+            this.lblPrecio.TabIndex = 4;
+            this.lblPrecio.Text = "Precio:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(90, 26);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(100, 20);
+            this.txtCodigo.TabIndex = 5;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(90, 52);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 6;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(90, 80);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(100, 20);
+            this.txtPrecio.TabIndex = 7;
+            // 
+            // txtStock
+            // 
+            this.txtStock.Location = new System.Drawing.Point(90, 112);
+            this.txtStock.Name = "txtStock";
+            this.txtStock.Size = new System.Drawing.Size(100, 20);
+            this.txtStock.TabIndex = 8;
+            // 
+            // gbInventario
+            // 
+            this.gbInventario.Controls.Add(this.lblCodigo);
+            this.gbInventario.Controls.Add(this.txtStock);
+            this.gbInventario.Controls.Add(this.lblNombre);
+            this.gbInventario.Controls.Add(this.txtPrecio);
+            this.gbInventario.Controls.Add(this.lblStock);
+            this.gbInventario.Controls.Add(this.txtNombre);
+            this.gbInventario.Controls.Add(this.lblPrecio);
+            this.gbInventario.Controls.Add(this.txtCodigo);
+            this.gbInventario.Location = new System.Drawing.Point(31, 72);
+            this.gbInventario.Name = "gbInventario";
+            this.gbInventario.Size = new System.Drawing.Size(200, 147);
+            this.gbInventario.TabIndex = 9;
+            this.gbInventario.TabStop = false;
+            this.gbInventario.Text = "Inventario";
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(273, 84);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(75, 23);
+            this.btnRegistrar.TabIndex = 10;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(273, 118);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(75, 23);
+            this.btnListar.TabIndex = 11;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(273, 152);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 12;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // btnSalir
+            // 
+            this.btnSalir.Location = new System.Drawing.Point(272, 190);
+            this.btnSalir.Name = "btnSalir";
+            this.btnSalir.Size = new System.Drawing.Size(75, 23);
+            this.btnSalir.TabIndex = 13;
+            this.btnSalir.Text = "Salir";
+            this.btnSalir.UseVisualStyleBackColor = true;
+            // 
+            // lstListado
+            // 
+            this.lstListado.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.lstListado.FormattingEnabled = true;
+            this.lstListado.Location = new System.Drawing.Point(375, 84);
+            this.lstListado.Name = "lstListado";
+            this.lstListado.Size = new System.Drawing.Size(377, 134);
+            this.lstListado.TabIndex = 14;
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(784, 295);
+            this.Controls.Add(this.lstListado);
+            this.Controls.Add(this.btnSalir);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.gbInventario);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "frmPrincipal";
+            this.Text = "AlmacenesBoff";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            this.gbInventario.ResumeLayout(false);
+            this.gbInventario.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblStock;
+        private System.Windows.Forms.Label lblPrecio;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtStock;
+        private System.Windows.Forms.GroupBox gbInventario;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnSalir;
+        private System.Windows.Forms.ListBox lstListado;
+    }
+}
+
